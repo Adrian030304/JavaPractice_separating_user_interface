38cc7f94020ca8494bc3b0d78b2b6e1bb301f625
Adrian030304 <adrianromanenco1@gmail.com>
2025-10-20 23:04:04 +0300

    grade register class revamp

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -10,35 +10,8 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         GradeRegister register = new GradeRegister();
 
-        while (true) {
-            System.out.println("Points: ");
-            String input = scanner.nextLine();
-            if (input.isEmpty()) {
-                break;
-            }
-            int score= Integer.valueOf(input);
-
-            if (score < 0 || score > 100) {
-                System.out.println("Impossible number");
-                continue;
-            }
-
-            register.addGradeBasedOnPoints(score);
-        }
-        System.out.println("");
-        int grade = 5;
-        while (grade >= 0) {
-            int stars = register.numberOfGrades(grade);
-            System.out.print(grade + ": ");
-            while (stars > 0) {
-                System.out.print("*");
-                stars--;
-            }
-            System.out.println("");
-
-            grade = grade - 1;
-        }
-        }
+        UserInterface userInterface = new UserInterface(register, scanner);
+        userInterface.start();
 }
 
 
--- src/main/java/org/example/UserInterface.java
+++ src/main/java/org/example/UserInterface.java
@@ -5,25 +5,52 @@ import java.util.Scanner;
 
 public class UserInterface {
     private Scanner scanner;
-    private WordSet words;
+    private GradeRegister register;
 
-    public UserInterface(WordSet words, Scanner scanner) {
+    public UserInterface(GradeRegister register, Scanner scanner) {
         this.scanner = scanner;
-        this.words = words;
+        this.register = register;
     }
 
     public void start() {
-        while (true) {
-            System.out.println("Enter a word");
-            String word = scanner.nextLine();
-
-            if (words.contains(word)) {
-                break;
-            }
-            this.words.add(word);
-        }
-        System.out.println("You already entered the word twice.");
-        System.out.println("There are " + words.palindromes() + " palindromes in the list.");
 
     }
+
+    void readPoints(){
+        while (true) {
+            System.out.println("Points: ");
+            String input = scanner.nextLine();
+            if (input.isEmpty()) {
+                break;
+            }
+            int score= Integer.valueOf(input);
+
+            if (score < 0 || score > 100) {
+                System.out.println("Impossible number");
+                continue;
+            }
+
+            register.addGradeBasedOnPoints(score);
+        }
+    }
+
+    void printGradeDistribution(){
+        int grade = 5;
+        while (grade >= 0) {
+            int stars = register.numberOfGrades(grade);
+            System.out.print(grade + ": ");
+            printStars(stars);
+            System.out.println("");
+
+            grade = grade - 1;
+        }
+    }
+
+    public static void printStars(int stars) {
+        while (stars > 0) {
+            System.out.print("*");
+            stars--;
+        }
+    }
 }
+
